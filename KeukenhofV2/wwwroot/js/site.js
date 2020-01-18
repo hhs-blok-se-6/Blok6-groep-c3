3cdfd56c57cdc497ac58f21dd7bac1fb77bce742
Joost <18064728@student.hhs.nl>
2020-01-18 13:23:04 +0100

    added webdeploy and added ccc/js files

--- KeukenhofV2/wwwroot/js/site.js
+++ KeukenhofV2/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//LEES: Alles dat onderstaande functie staat, wordt automatisch uitgevoerd bij het opstarten van de pagina.
+//Dit is de meest fijne manier om met Javascript te werken, omdat je hierdoor niet zelf in HTML nog javascript functies hebt aangeroepen
+$(document).ready(function () {
 
-// Write your JavaScript code.
+});
+
+var x = document.getElementById("search-bar"); /* the search bar */
+var cover = document.getElementById("overlay"); /* the whole page */
+
+/*  displays a search bar and darkens the rest of the site */
+function on() {
+    x.style.display = "flex";
+    cover.style.display = "block";
+}
+
+/* hides the search bar and lightens the site when clicked elsewhere or 'Annuleer' */
+function off() {
+    x.style.display = "none";
+    document.getElementById('search').value = "";
+    cover.style.display = "none";
+}
+
+$(document).ready(function () {
+    $(".accordion h1").click(function () {
+        var id = this.id;
+        $(".accordion-content").each(function () {
+            if ($("#" + id).next()[0].id != this.id) {
+                $(this).slideUp("slow");
+            }
+        });
+
+        $(this).next().toggle("slow");
+    });
+}); 
