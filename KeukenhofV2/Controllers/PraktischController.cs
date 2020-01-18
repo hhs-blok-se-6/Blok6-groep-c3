a3abf86dd68d10c5b993971a53dab3573de0f530
Tomas <tomaszwanikken@gmail.com>
2020-01-18 13:13:59 +0100

    Toegankelijkheid

--- KeukenhofV2/Controllers/PraktischController.cs
+++ KeukenhofV2/Controllers/PraktischController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KeukenhofV2.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KeukenhofV2.Controllers
 {
     public class PraktischController : Controller
     {
-        public IActionResult Index()
+        private readonly KeukenhofContext _context;
+
+        public PraktischController(KeukenhofContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Toegankelijkheid()
+        {
+            var toegankelijkheidContents = from tc in _context.EvenementenContent select tc;
+
+            return View("Toegankelijkheid", await toegankelijkheidContents.AsNoTracking().ToListAsync());
         }
     }
 }
---
+++ KeukenhofV2/Models/ToegankelijkheidContents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeukenhofV2.Models
+{
+    public class ToegankelijkheidContents
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+    }
+}
