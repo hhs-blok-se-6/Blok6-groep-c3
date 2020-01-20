9e59fc260452a494ba99b9368bbee8d57bf64c6b
Joost <18064728@student.hhs.nl>
2020-01-20 23:57:06 +0100

    Added ctaButton model, added related images.

---
+++ KeukenhofV2/Models/CTAButton.cs
@@ -0,0 +1,11 @@
+﻿namespace KeukenhofV2.Models
+{
+    public class CTAButton
+    {
+        public int Id { get; set; }
+        public string Page { get; set; }
+        public string Image { get; set; }
+        public string Text { get; set; }
+        public string Link { get; set; }
+    }
+}
