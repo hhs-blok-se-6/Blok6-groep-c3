d8a16cbdd3023d31991ca687068a57fa2708ba40
Joost <18064728@student.hhs.nl>
2020-01-24 00:09:26 +0100

    added privacy page

--- KeukenhofV2/Controllers/PrivacyController.cs
+++ KeukenhofV2/Controllers/PrivacyController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using KeukenhofV2.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using KeukenhofV2.Data;
-using KeukenhofV2.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
 
 
 
@@ -24,9 +21,11 @@ namespace KeukenhofV2.Controllers
 
         [Route("/Privacy")]
         [Route("/Privacy/Index")]
-        public IActionResult Index()
+        public async Task<IActionResult> Privacy()
         {
-            return View();
+            var privacyContent = from pc in _context.PrivacyContent select pc;
+
+            return View("Privacy", await privacyContent.AsNoTracking().ToListAsync());
         }
 
         [Authorize]
