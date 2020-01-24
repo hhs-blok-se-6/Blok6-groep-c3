e2e6a323aa93634fb67acde0d78be6e74a1d8c16
Joost <18064728@student.hhs.nl>
2020-01-24 09:00:56 +0100

    add pagination for evenementen page

--- KeukenhofV2/Controllers/EvenementenController.cs
+++ KeukenhofV2/Controllers/EvenementenController.cs
@@ -1,8 +1,11 @@
 ﻿using KeukenhofV2.Data;
+using KeukenhofV2.Models;
+using KeukenhofV2.ViewModel;
 using KeukenhofV2.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,7 +19,14 @@ namespace KeukenhofV2.Controllers
         {
             _context = context;
         }
-        [Route("Evenementen")]
+
+        private IQueryable<CardContent> GetLijst()
+        {
+            return from cc in _context.CardContent where cc.Page.Equals("Evenementen") select cc;
+        }
+
+        [Route("/Evenementen")]
+        [Route("/Evenementen/Index")]
         public async Task<IActionResult> Evenementen()
         {
             var evenementenPaginaContent = from ec in _context.EvenementenPagina select ec;
@@ -27,6 +37,34 @@ namespace KeukenhofV2.Controllers
             return View("Evenementen", model);
         }
 
+        public async Task<IActionResult> Evenementen(string newSearchVal, string searchVal, int? pagina)
+        {
+
+            if (String.IsNullOrEmpty(newSearchVal))
+                newSearchVal = searchVal;
+
+            else
+                pagina = 1;
+            var lijst = GetLijst();
+            EvenementenViewModel epvm = new EvenementenViewModel()
+            {
+                evenementen = lijst,
+                Rows = 2,
+                Columns = 4
+            };
+
+
+            return View(await Paging(lijst, pagina));
+        }
+
+        private async Task<PaginatedList<CardContent>> Paging(IQueryable<CardContent> lijst, int? pagina)
+        {
+            pagina = pagina ?? 1;
+            ViewData["CurrentPage"] = pagina;
+            int aantalItemsPerPagina = 8;
+            return await PaginatedList<CardContent>.CreateAsync(lijst.AsNoTracking(), (int)pagina, aantalItemsPerPagina);
+        }
+
         public async Task<IActionResult> BloemenFestival()
         {
             var evenementenContent = from ec in _context.EvenementenContent select ec;
@@ -35,6 +73,7 @@ namespace KeukenhofV2.Controllers
         }
 
         [Authorize]
+        [Route("/Evenementen/Edit")]
         public async Task<IActionResult> Edit()
         {
             var evenementContent = from hc in _context.EvenementenContent select hc;
---
+++ KeukenhofV2/ViewModel/EvenementenViewModel.cs
@@ -0,0 +1,12 @@
+﻿using KeukenhofV2.Models;
+using System.Collections.Generic;
+
+namespace KeukenhofV2.ViewModel
+{
+    public class EvenementenViewModel
+    {
+        public IEnumerable<CardContent> evenementen { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+    }
+}
